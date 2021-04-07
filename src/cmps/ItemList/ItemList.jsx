917f26497c6c80742bc51d8afc5e7b7a89cd9a3e
dorrang <summ.pie@gmail.com>
2021-04-07 16:48:41 +0300

    cscc

--- src/cmps/ItemList/ItemList.jsx
+++ src/cmps/ItemList/ItemList.jsx
@@ -9,7 +9,7 @@ export class ItemList extends Component {
     render() {
         const { items, onSelectItem ,onAddToCart } = this.props
         return (
-            items && <div >
+            items && <div className="list-container" >
                 <ul className="item-list flex">
                     {items.map(item => <li className="item-preview-li" key={item._id}><ItemPreview onAddToCart={onAddToCart} onSelectItem={onSelectItem} key={item._id} item={item} /></li>)}
                 </ul>
