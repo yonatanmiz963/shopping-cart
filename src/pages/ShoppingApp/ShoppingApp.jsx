ef9140614385ba79e7ad9f4d14aead4d7ab432a8
Roi Levy <levy6161@gmail.com>
2021-04-07 16:57:59 +0300

    paging

--- src/pages/ShoppingApp/ShoppingApp.jsx
+++ src/pages/ShoppingApp/ShoppingApp.jsx
@@ -12,8 +12,8 @@ export class ShoppingApp extends Component {
     state = {
         items: null,
         filterBy: null,
-        page:1,
-        itemsPerPage:6
+        page: 1,
+        itemsPerPage: 6
     }
 
     componentDidMount() {
@@ -26,9 +26,14 @@ export class ShoppingApp extends Component {
     }
 
     async loadItems() {
-        const items = await itemService.getItems(this.state.filterBy)
-        items.splice()
-        this.setState({ items })
+        var items = await itemService.getItems(this.state.filterBy)
+        var itemCopy = [...items]
+        console.log('page:', (this.state.itemsPerPage * this.state.page) - 6)
+        console.log('page:', this.state.itemsPerPage * this.state.page)
+        const itemsToShow=itemCopy.splice((this.state.itemsPerPage * this.state.page) - 6, this.state.itemsPerPage * this.state.page)
+        console.log('items:', items)
+        console.log('itemsToShow:', itemsToShow)
+        this.setState({ items: itemsToShow })
     }
 
 
@@ -38,6 +43,13 @@ export class ShoppingApp extends Component {
 
     onAddToCart = (item) => {
         itemService.addToCart(item);
+    }
+
+    nextPage = () => {
+        this.setState(prevState => ({ page: ++prevState.page }), this.loadItems)
+    }
+    prevPage = () => {
+        this.setState(prevState => ({ page: --prevState.page }), this.loadItems)
     }
 
     render() {
@@ -50,6 +62,9 @@ export class ShoppingApp extends Component {
                     <Link className="add-btn" to="/item/edit">Add item</Link>
                 </div>
                 <ItemList onAddToCart={this.onAddToCart} onSelectitem={onSelectitem} items={items} />
+                <button onClick={this.prevPage}>Previous Page</button>
+                {this.state.page}
+                <button onClick={this.nextPage}>Next Page</button>
             </div>
         )
     }
