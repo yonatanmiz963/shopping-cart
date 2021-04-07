681b7a982c06456cbacddd0b74ff589eb242c358
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-04-07 17:00:17 +0300

    first

---
+++ src/cmps/CartPreview/CartPreview.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export function CartPreview({item}) {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        {/* <Typography className={classes.title} color="textSecondary" gutterBottom>
+        </Typography> */}
+        <Typography variant="h5" component="h2">
+          {item.title}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+        {item.price}
+        </Typography>
+        {/* <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography> */}
+      </CardContent>
+      <CardActions>
+        <Button size="small">Remove</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 
 import { Component } from 'react'
+import { CartPreview } from '../../cmps/CartPreview/CartPreview'
 import { ItemPreview } from '../../cmps/ItemPreview'
 import { itemService } from '../../services/itemService'
 
@@ -23,7 +24,8 @@ export class Cart extends Component {
         if (!cartItems) return <div>Loading...</div>
         return (
             <div className="cart-list-items">
-                {cartItems.map(item => <li className="item-preview-li" key={item._id}><ItemPreview key={item._id} item={item} /></li>)}
+                {/* {cartItems.map(item => <li className="item-preview-li" key={item._id}><ItemPreview key={item._id} item={item} /></li>)} */}
+                {cartItems.map(item => <li className="cart-preview-li" key={item._id}><CartPreview key={item._id} item={item} /></li>)}
             </div>
         )
     }
